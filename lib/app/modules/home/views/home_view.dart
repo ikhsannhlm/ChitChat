8296da214af866ed8616ef9a279365db77df2d71
ikhsannhlm <muhammad.ikhsan.n0192@gmail.com>
2023-08-08 10:32:45 +0700

    Creating Realtime Chats

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,3 +1,5 @@
+import 'package:chitchat/app/controllers/auth_controller.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
@@ -6,35 +8,8 @@ import '../../../routes/app_pages.dart';
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
-  final List<Widget> myChats = List.generate(
-    15,
-    (index) => ListTile(
-      onTap: () => Get.toNamed(Routes.CHAT_ROOM),
-      leading: CircleAvatar(
-        radius: 30,
-        backgroundColor: Colors.black26,
-        child: Image.asset(
-          "assets/logo/noimage.png",
-          fit: BoxFit.cover,
-        ),
-      ),
-      title: Text(
-        "Orang ke ${index + 1}",
-        style: TextStyle(
-          fontSize: 20,
-          fontWeight: FontWeight.w600,
-        ),
-      ),
-      subtitle: Text(
-        "Status orang ke ${index + 1}",
-        style: TextStyle(
-          fontSize: 16,
-          fontWeight: FontWeight.w600,
-        ),
-      ),
-      trailing: Chip(label: Text("3")),
-    ),
-  ).reversed.toList();
+  final authC = Get.find<AuthController>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -90,10 +65,47 @@ class HomeView extends GetView<HomeController> {
             ),
           ),
           Expanded(
-            child: ListView.builder(
-              padding: EdgeInsets.zero,
-              itemCount: myChats.length,
-              itemBuilder: (context, index) => myChats[index],
+            child: StreamBuilder<DocumentSnapshot<Map<String, dynamic>>>(
+              stream: controller.chatsStream(authC.user.value.email!),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.active) {
+                  var allChats = (snapshot.data!.data()
+                      as Map<String, dynamic>)["chats"] as List;
+                  return ListView.builder(
+                    padding: EdgeInsets.zero,
+                    itemCount: allChats.length,
+                    itemBuilder: (context, index) => ListTile(
+                      onTap: () => Get.toNamed(Routes.CHAT_ROOM),
+                      leading: CircleAvatar(
+                        radius: 30,
+                        backgroundColor: Colors.black26,
+                        child: Image.asset(
+                          "assets/logo/noimage.png",
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      title: Text(
+                        "Orang ke ${index + 1}",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      subtitle: Text(
+                        "Status orang ke ${index + 1}",
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      trailing: Chip(label: Text("3")),
+                    ),
+                  );
+                }
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              },
             ),
           ),
         ],
