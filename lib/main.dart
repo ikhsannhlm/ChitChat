efc0342b0dddcf28835d15f347873a161886e2e2
ikhsannhlm <muhammad.ikhsan.n0192@gmail.com>
2023-08-11 15:19:01 +0700

    Feature Device Orientation

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
-
+import 'package:flutter/services.dart';
 import 'app/routes/app_pages.dart';
 import 'app/utils/splash_screen.dart';
 import 'app/controllers/auth_controller.dart';
@@ -11,7 +11,8 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   await GetStorage.init();
-  runApp(MyApp());
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((value) => runApp(MyApp()));
 }
 
 class MyApp extends StatelessWidget {
